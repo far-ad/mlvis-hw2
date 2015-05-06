bd33afbde3edc4dbf5a2f0cca08a94a045199611
Raphael Dümig <duemig@in.tum.de>
2015-05-06 14:40:19 +0200

    added cost function
    classifier only executed of module is main module

--- classifier.py
+++ classifier.py
@@ -20,24 +20,47 @@ PRETRAINED = caffe_root + 'models/bvlc_reference_caffenet/bvlc_reference_caffene
 IMAGE_FILES = sorted(map(lambda f: 'images/' + f, os.listdir('images')))
 SYNSET_FILE = caffe_root + 'data/ilsvrc12/synset_words.txt'
 
-classes = list(map(lambda l: ' '.join(l.split()[1:]), open(SYNSET_FILE)))
+def costs(class_ind, prediction):
+	res = 0
 
-caffe.set_mode_cpu()
-net = caffe.Classifier(MODEL_FILE, PRETRAINED,
-                       mean=np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1),
-                       channel_swap=(2,1,0),
-                       raw_scale=255,
-                       image_dims=(256, 256))
+	i = 0
+	for prob in prediction:
+		if i == class_ind:
+			res += (prob - 1)**2
+		else:
+			res += prob**2
+	
+	return res
 
-input_images = map(lambda img_path: caffe.io.load_image(img_path), IMAGE_FILES)
-#plt.imshow(input_images)
+if __name__ == '__main__':
+	classes = list(map(lambda l: ' '.join(l.split()[1:]), open(SYNSET_FILE)))
 
-predictions = net.predict(input_images)  # predict takes any number of images, and formats them for the Caffe net automatically
+	caffe.set_mode_cpu()
+	net = caffe.Classifier(MODEL_FILE, PRETRAINED,
+			       mean=np.load(caffe_root + 'python/caffe/imagenet/ilsvrc_2012_mean.npy').mean(1).mean(1),
+			       channel_swap=(2,1,0),
+			       raw_scale=195,
+			       image_dims=(256, 256))
 
-for prediction in predictions:
-	#plt.plot(prediction)
-	print('predicted class: %s' % classes[prediction.argmax()])
-	print('predicted probability: %.3f' % max(prediction))
-	print('predicted entropy: %.3f' % entropy(prediction))
-	print('')
+	print('==> loading images...')
+	input_images = map(lambda img_path: caffe.io.load_image(img_path), IMAGE_FILES)
+	input_classes = [336, None, None, None, None, 948, 458, None]
+	#plt.imshow(input_images)
+
+	print('==> finished loading')
+	print('==> analyzing images')
+	predictions = net.predict(input_images)  # predict takes any number of images, and formats them for the Caffe net automatically
+
+	# i: image counter
+	i = 0
+	for prediction in predictions:
+		#plt.plot(prediction)
+		print('image %d:' % i)
+		print('\tpredicted class: %s' % classes[prediction.argmax()])
+		print('\tpredicted probability: %.3f' % max(prediction))
+		print('\tpredicted entropy: %.3f' % entropy(prediction))
+		if input_classes[i] is not None:
+			print('\tclassification costs: %.3f' % costs(input_classes[i]-1, prediction))
+		print('')
+		i += 1
 
